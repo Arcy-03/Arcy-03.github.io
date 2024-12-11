100c3f5804eaff1c3c7632592e8ee9fde58c644c
Arcy03 <hc22aav@herts.ac.uk>
2024-12-11 02:44:36 +0000

    changes again

--- js/trial.js
+++ js/trial.js
@@ -1,3 +1,4 @@
 // enlarge the h1 font size
 document.getElementsByTagName("h1")[0].style.fontSize = "4.5vw";
 document.getElementsByTagName("h2")[0].style.fontSize = "1.25vw";
+document.getElementsByTagName("h3")[0].style.fontSize = "1.25vw";
